97cb4b524b2bc3640bb67d5030c84ee979bc3444
Federico Carro <federico.carro@alu.inspt.utn.edu.ar>
2024-04-17 22:15:21 -0300

    commit 4

---
+++ Operadores.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define p printf
+#define s scanf
+int main(void)
+{
+    int num1, num2, resultado;
+    num1 = 1;
+    num2 = 2;
+    p("Ingrese un numero: ");
+    s("%d", &num1);
+    p("Ingrese otro numero: ");
+    s("%d", &num2);
+    // resultado= num1%2;
+    if (num1 > num2)
+    {
+        p("\n%d es mayor que %d", num1, num2);
+    }
+    else if (num1 == num2)
+    {
+        p("\n%d es igual que %d", num1, num2);
+    }
+    else
+    {
+        p("\n%d es mayor que %d", num2, num1);
+    }
+
+    return 0;
+}
